6d247281c91f2fbb76aa09487a6b7e3f8c895d40
Mohammad-Naqiuddin-Mubeen <mohammadnaqi619@gmail.com>
2024-09-29 03:42:51 +0530

    Some Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Master } from './components/Master.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <Master></Master>
+  </>
 )
